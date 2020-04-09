6a6ba7df46f5b0b7f8cfa3c729e03c61e9dde45f
Codax2000 <56089279+Codax2000@users.noreply.github.com>
2020-04-08 20:11:11 -0700

    supplementary files for testing

---
+++ banner_test_force_airspeed_v2.ino
@@ -0,0 +1,117 @@
+/*
+ Example using the SparkFun HX711 breakout board with a scale
+ By: Nathan Seidle
+ SparkFun Electronics
+ Date: November 19th, 2014
+ License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
+
+ This is the calibration sketch. Use it to determine the calibration_factor that the main example uses. It also
+ outputs the zero_factor useful for projects that have a permanent mass on the scale in between power cycles.
+
+ Setup your scale and start the sketch WITHOUT a weight on the scale
+ Once readings are displayed place the weight on the scale
+ Press +/- or a/z to adjust the calibration_factor until the output readings match the known weight
+ Use this calibration_factor on the example sketch
+
+ This example assumes pounds (lbs). If you prefer kilograms, change the Serial.print(" lbs"); line to kg. The
+ calibration factor will be significantly different but it will be linearly related to lbs (1 lbs = 0.453592 kg).
+
+ Your calibration factor may be very positive or very negative. It all depends on the setup of your scale system
+ and the direction the sensors deflect from zero state
+ This example code uses bogde's excellent library:"https://github.com/bogde/HX711"
+ bogde's library is released under a GNU GENERAL PUBLIC LICENSE
+ Arduino pin 2 -> HX711 CLK
+ 3 -> DOUT
+ 5V -> VCC
+ GND -> GND
+
+ Most any pin on the Arduino Uno will be compatible with DOUT/CLK.
+
+ The HX711 board can be powered from 2.7V to 5V so the Arduino 5V power should be fine.
+
+*/
+
+#include "HX711.h"
+#include "Servo.h"
+
+#define CLAMP 6
+#define DOUT  3
+#define CLK  2
+
+HX711 scale;
+
+Servo clamp;
+int pos = 0;
+
+float calibration_factor = -33300; //-7050 worked for my 440lb max scale setup
+float V_0 = 5.0;
+float rho = 1.204;
+
+// parameters for averaging
+int offset = 0; 
+int offset_size = 10;
+int veloc_mean_size = 20;
+int zero_span = 2;
+
+void setup() {
+//  clamp.attach(CLAMP);\
+//  clamp.write(0);
+  Serial.begin(9600);
+  Serial.println("HX711 calibration sketch");
+  Serial.println("Remove all weight from scale");
+  Serial.println("After readings begin, place known weight on scale");
+  Serial.println("Press + or a to increase calibration factor");
+  Serial.println("Press - or z to decrease calibration factor");
+
+  scale.begin(DOUT, CLK);
+  scale.set_scale();
+  scale.tare(); //Reset the scale to 0
+
+  long zero_factor = scale.read_average(); //Get a baseline reading
+  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+  Serial.print(zero_factor + "\t");
+
+  for (int ii = 0; ii < offset_size; ii++) {
+    offset += analogRead(A0) - (1023 / 2);
+    
+  }
+  offset /= offset_size;
+}
+
+void loop() {
+
+  scale.set_scale(calibration_factor); //Adjust to this calibration factor
+
+  //Serial.print("Reading: ");
+  Serial.print(scale.get_units(), 3);
+  //Serial.print(" lbs"); //Change this to kg and re-adjust the calibration factor if you follow SI units like a sane person
+  //Serial.print(" calibration_factor: ");
+  //Serial.print(calibration_factor);
+  //Serial.print("\t\t");
+  Serial.print(",");
+
+  
+
+  float adc_avg = 0;
+  float veloc = 0.0;
+
+  //average a few for stability
+  for (int ii = 0; ii < veloc_mean_size; ii++) {
+    adc_avg += analogRead(A0) - offset;
+  }
+  adc_avg /= veloc_mean_size;
+  
+  // make sure if the ADC reads below 512, then we equate it to a negative velocity
+  if (adc_avg>512-zero_span and adc_avg<512+zero_span){
+  } else{
+    if (adc_avg<512){
+      veloc = -sqrt((-10000.0*((adc_avg/1023.0)-0.5))/rho);
+    } else{
+      veloc = sqrt((10000.0*((adc_avg/1023.0)-0.5))/rho);
+    }
+  }
+  
+  Serial.println(veloc * 2.237); // print velocity in miles per hour
+  delay(100); // delay for stability
+  
+}
